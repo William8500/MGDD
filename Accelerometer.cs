25e65203c8b791e2c6006d14b65c0e8ceef9661a
William8500 <62640433+William8500@users.noreply.github.com>
2020-07-21 15:20:03 +0800

    Add files via upload

---
+++ Accelerometer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public float accelerateSpeed = 5;
+    float speedMin = -20;
+    float speedMax = 20;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.acceleration.x;
+        float movementY = Input.acceleration.y;
+        float movementZ = 0;
+        transform.Translate(accelerateSpeed*movementX*Time.deltaTime,
+                            accelerateSpeed*movementY*Time.deltaTime,
+                            accelerateSpeed*movementZ*Time.deltaTime);
+
+        if (accelerateSpeed >= speedMax)
+            {
+            accelerateSpeed = speedMax;
+            }
+
+        if(accelerateSpeed <= speedMin)
+        {
+            accelerateSpeed = speedMin;
+        }
+    }
+}
